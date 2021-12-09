52218bb9c11851c45c1e22603356584cefb44195
szaszgergo <71765638+szaszgergo@users.noreply.github.com>
2021-12-09 09:39:37 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,114 @@
+
+from math import *
+from random import * 
+
+
+def feladat1():
+    a = "asdadsadsasdasdasdasdasdasdadasdasd"
+
+def feladat2():
+    b = "a"
+def feladat3():
+    c =  2  
+def feladat4():
+    d = 2.5
+def feladat5():
+    e = True
+def feladat6():
+    szo = input("Kérek egy szót!")
+    tarolt = szo
+def feladat7():
+    mondat = input("Kérj be egy mondatot és tárold el!")
+    tarolt_mondat= mondat
+def feladat8():
+    karakter = input("Kérj be egy karaktert és tárold el!")
+def feladat9():
+    egeszszam = int(input("Kérj be egy egész számot és tárold el!"))
+def feladat10():
+    valosszam = float(input(" Kérj be egy valós számot és tárold el!"))
+    ketszeres = valosszam*2
+    print(ketszeres)
+    piszeres = valosszam*pi
+    print(piszeres)
+    hatvany = valosszam**3
+    print(hatvany)
+    hatvany = pow(valosszam,3)
+    print(hatvany)
+    negyzet = sqrt(valosszam)
+    print(negyzet)
+
+    negyzetgyokkerekit =round( negyzet,2)
+    print(negyzetgyokkerekit)
+def feladat11():
+    a=randint(10,50)
+def feladat12():
+    a = int(input("Kérek egy számot "))
+    if a%2==0:
+        print("páros")
+    else:
+        print('Páratlan')
+def feladat13():
+    a = int(input("Kérek egy számot "))
+    if a>0:
+        print("pozitív")
+    elif a < 0:
+        print("Negatív")
+    else:
+        print("Nulla")
+def feladat14():
+    a = int(input("Kérek egy számot "))
+    if a >30:
+        print("a szám nagyobb mint 30")
+    elif a == 30 : 
+        print("a szám 0 ")
+    else:
+        print(" a szám nem nagyobb mint 30 ")
+def feladat15():
+    a = int(input("Kérem a  számot "))
+    b = int(input("Kérem b  számot "))
+    if a > b :
+        print("a nagyobb mint b ")
+    elif a< b : 
+        print(" b nagyobb mint a ")
+    else:
+        print(" a két szám egyenlő ")
+def feladat16():
+        
+    a = int(input("Kérem a  számot "))
+    b = int(input("Kérem b  számot "))
+    if a > b :
+        print("b kisebb mint a   ")
+    elif a< b : 
+        print(" a kisebb mint b  ")
+    else:
+        print(" a két szám egyenlő ")
+def feladat17():
+    a = input("Kérek egy szót")
+    print(len(a))
+def feladat18():
+    a = input("Kérek egy szót")
+    ee = a.count("e")
+print("Ennyi e van benne", ee)
+def feladat19():
+
+
+
+
+feladat1()
+feladat2()
+feladat3()
+feladat4()
+feladat5()
+feladat6()
+feladat7()
+feladat8()
+feladat9()
+feladat10()
+feladat12()
+feladat13()
+feladat14()
+feladat15()
+feladat16()
+feladat17()
+feladat18()
+feladat19()
