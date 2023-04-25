f1c05fdea879695072f6d9435a342329cb0dafed
szaszgergo <71765638+szaszgergo@users.noreply.github.com>
2023-04-25 11:25:08 +0200

    Add files via upload

---
+++ 20230425/20230425/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20230425
+{
+    class Program
+    {
+        static int[] lista = new int[50];
+        static List<int> oszthato = new List<int>();
+        static List<int> nemoszthato = new List<int>();
+        static Random rnd = new Random();
+
+        static void Main(string[] args)
+        {
+            feltoltes();
+            eldontes();
+            Console.WriteLine("Nyomd le az entert!");
+            Console.ReadLine();
+        }
+        static void feltoltes()
+        {
+            for (int i = 0; i < lista.Length; i++)
+            {
+                int fel = rnd.Next(0, 50);
+                Console.Write($"{fel}, ");
+                lista[i]=fel;
+            }
+         
+        }
+
+        static void eldontes()
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Oszthatóak 3-al és maradék 2: ");
+            for (int i = 0; i < lista.Length; i++)
+            {
+                if (lista[i]%3==2)
+                {
+                    oszthato.Add(lista[i]);
+                }
+                else
+                {
+                    nemoszthato.Add(lista[i]);
+                }
+               
+            }
+            foreach (var item in oszthato)
+            {
+                Console.Write($"{item}, ");
+            }
+        }
+       
+    }
+
+}
